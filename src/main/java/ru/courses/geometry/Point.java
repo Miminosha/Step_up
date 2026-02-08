e22a29201c0bd5d0fec5a2f438907d3035de296b
Anna Memikova <anna.memikova@yandex.ru>
2026-02-08 18:34:49 +0300

    Практика ООП. Класс Object №2. Сравнение точек

---
+++ src/main/java/ru/courses/geometry/Point.java
@@ -0,0 +1,34 @@
+package ru.courses.geometry;
+
+import java.util.Objects;
+
+public class Point implements Cloneable {
+    private final int x, y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x && y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    @Override
+    public Point clone() throws CloneNotSupportedException {
+        return (Point) super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return "Point {" + x + ";" + y + '}';
+    }
+}
--- src/main/java/ru/courses/main/Main.java
+++ src/main/java/ru/courses/main/Main.java
@@ -1,7 +1,15 @@
 package ru.courses.main;
 
+import ru.courses.geometry.Point;
+
 public class Main {
-    static void main() {
-        System.out.println("Hello!");
+    static void main() throws Exception {
+
+        Point p1 = new Point(1, 5);
+        Point p2 = p1.clone();
+        System.out.println(p1 == p2);
+        System.out.println(p1.equals(p2));
+        System.out.println(p1);
+        System.out.println(p2);
     }
 }

30a5512977321a833b11cf52449aa6037bc11ead
Anna Memikova <anna.memikova@yandex.ru>
2026-02-08 18:51:39 +0300

    Практика ООП. Класс Object №1. Сравнение дробей

--- src/main/java/ru/courses/main/Main.java
+++ src/main/java/ru/courses/main/Main.java
@@ -1,7 +1,15 @@
 package ru.courses.main;
 
+import ru.courses.math.Fraction;
+
 public class Main {
-    static void main() {
-        System.out.println("Hello!");
+    static void main() throws Exception {
+        Fraction f1 = new Fraction(3, 7);
+        Fraction f2 = f1.clone();
+
+        System.out.println(f1 == f2);
+        System.out.println(f1.equals(f2));
+        System.out.println(f1);
+        System.out.println(f2);
     }
 }
---
+++ src/main/java/ru/courses/math/Fraction.java
@@ -0,0 +1,33 @@
+package ru.courses.math;
+
+import java.util.Objects;
+
+public class Fraction implements Cloneable {
+    private final int num, denum;
+
+    public Fraction(int num, int denum) {
+        this.num = num;
+        this.denum = denum;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Fraction fraction = (Fraction) o;
+        return num == fraction.num && denum == fraction.denum;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(num, denum);
+    }
+
+    @Override
+    public Fraction clone() throws CloneNotSupportedException {
+        return (Fraction) super.clone();
+    }
+
+    public String toString() {
+        return num + "/" + denum;
+    }
+}

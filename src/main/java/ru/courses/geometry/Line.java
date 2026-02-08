a9f47688777d1909b44afc8fcdbb112204f4fac4
Anna Memikova <anna.memikova@yandex.ru>
2026-02-08 19:04:16 +0300

    Практика ООП. Класс Object №3. Сравнение линий

---
+++ src/main/java/ru/courses/geometry/Line.java
@@ -0,0 +1,34 @@
+package ru.courses.geometry;
+
+import java.util.Objects;
+
+public class Line implements Cloneable {
+    Point start, end;
+
+    public Line(Point start, Point end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Line line = (Line) o;
+        return Objects.equals(start, line.start) && Objects.equals(end, line.end);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(start, end);
+    }
+
+    @Override
+    public Line clone() throws CloneNotSupportedException {
+        return (Line) super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return "Line {" + "start" + start + ", end" + end + '}';
+    }
+}
--- src/main/java/ru/courses/main/Main.java
+++ src/main/java/ru/courses/main/Main.java
@@ -1,5 +1,6 @@
 package ru.courses.main;
 
+import ru.courses.geometry.Line;
 import ru.courses.geometry.Point;
 
 public class Main {
@@ -7,9 +8,13 @@ public class Main {
 
         Point p1 = new Point(1, 5);
         Point p2 = p1.clone();
-        System.out.println(p1 == p2);
-        System.out.println(p1.equals(p2));
-        System.out.println(p1);
-        System.out.println(p2);
+
+        Line l1 = new Line(p1, p2);
+        Line l2 = l1.clone();
+
+        System.out.println(l1 == l2);
+        System.out.println(l1.equals(l2));
+        System.out.println(l1);
+        System.out.println(l2);
     }
 }

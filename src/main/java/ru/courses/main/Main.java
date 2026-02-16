900040964ad89c18d6ebda20a506cfc4b6d298ff
Anna Memikova <anna.memikova@yandex.ru>
2026-02-16 23:35:46 +0300

    Попарные перестановки

--- src/main/java/ru/courses/main/Main.java
+++ src/main/java/ru/courses/main/Main.java
@@ -1,7 +1,21 @@
 package ru.courses.main;
 
+import java.util.ArrayList;
+
 public class Main {
     static void main() {
-        System.out.println("Hello!");
+        int N = 10;
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i = 1; i <= N; i++) {
+            arrayList.add(i);
+        }
+        System.out.println(arrayList);
+
+        for (int i = 0; i < N; i += 2) {
+            int temp = arrayList.get(i);
+            arrayList.set(i, arrayList.get(i + 1));
+            arrayList.set(i + 1, temp);
+        }
+        System.out.println(arrayList);
     }
 }

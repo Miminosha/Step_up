7dfbcf6ddbcb91a90c6baf242b76527af8e3d40d
Anna Memikova <anna.memikova@yandex.ru>
2026-02-19 01:38:42 +0300

    Замена массива на ArrayList

--- src/main/java/ru/courses/main/Main.java
+++ src/main/java/ru/courses/main/Main.java
@@ -1,7 +1,26 @@
 package ru.courses.main;
 
+import java.util.ArrayList;
+
 public class Main {
     static void main() {
-        System.out.println("Hello!");
+    }
+
+    public static void reverse(int[] intArray) {
+        int n = intArray.length - 1;
+        for (int i = 0; i < intArray.length / 2; i++) {
+            int temp = intArray[i];
+            intArray[i] = intArray[n - i];
+            intArray[n - i] = temp;
+        }
+    }
+
+    public static void reverse(ArrayList<Integer> intList) {
+        int n = intList.size() - 1;
+        for (int i = 0; i < intList.size() / 2; i++) {
+            int temp = intList.get(i);
+            intList.set(i, intList.get(n - 1));
+            intList.set(n - 1, temp);
+        }
     }
 }

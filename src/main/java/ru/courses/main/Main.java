3023426801a64ff29975907771a440c47d314f8d
Anna Memikova <anna.memikova@yandex.ru>
2026-03-09 22:41:01 +0300

    Преобразование потока строк в строку

--- src/main/java/ru/courses/main/Main.java
+++ src/main/java/ru/courses/main/Main.java
@@ -1,7 +1,16 @@
 package ru.courses.main;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
     static void main() {
-        System.out.println("Hello!");
+        Stream<String> stream = Stream.of("I", "like", "java");
+        String result = getStringFromStream(stream);
+        System.out.println(result);
+    }
+
+    public static String getStringFromStream(Stream<String> stringStream) {
+        return stringStream.collect(Collectors.joining(" "));
     }
 }
